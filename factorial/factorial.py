886cee336568cbe4a1d0a615ddff391860724eb5
jfertri853 <jfertri853@g.educaand.es>
2023-10-19 21:25:47 +0200

    ambos factoriales

--- factorial/factorial.py
+++ factorial/factorial.py
@@ -1,7 +1,28 @@
-def factorial(num):
+def factorial(num: int):
+    if num == 0:
+        return 1
+
     for i in range(num - 1, 0, -1):
         num *= i
     return num
 
+
+def factorial_str(num: int):
+    if num == 0:
+        return 1
+
+    cadena = str(num) + "!" + " => "
+    resultado = 1
+    for i in range(num, 0, -1):
+        cadena += str(i)
+        if i != 1:
+            cadena += " x "
+        else:
+            cadena += " = "
+        resultado *= i
+    return cadena + str(resultado)
+
+
 numero = int(input("Introduce un numero: "))
 print(factorial(numero))
+print(factorial_str(numero))
